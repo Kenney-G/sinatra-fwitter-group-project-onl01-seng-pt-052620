9d8c48f0efda7009967c271a073da6d51b37a999
kenney-g <goadke@g.cofc.edu>
2020-09-15 00:53:51 -0400

    Add current_user and logged_in methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :sessions, true
+    set :session_secret, ENV["SESSION_SECRET"]
   end
 
+def current_user
+  @current_user = User.find_by(session[:id])
+end
+
+def logged_in?
+  !!current_user
 end
